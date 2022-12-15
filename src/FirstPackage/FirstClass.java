5991238dfc95159c3f5413875bedc02b5625cc64
Lamia <kessoumlamia@gmail.com>
2022-12-15 18:22:52 -0500

    initial commit

---
+++ src/FirstPackage/FirstClass.java
@@ -0,0 +1,25 @@
+package FirstPackage;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        String x = "Romel";
+        //initializing the variable
+        int a,b,c;
+        //declaring the variable
+        a= 10;
+        b= 20;
+        c= 30;
+        //initialize and declare in same line
+        int d=40;
+        System.out.println(a+b);
+    }
+
+
+
+
+    }
+
+
+
+
